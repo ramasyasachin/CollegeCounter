23c0a927d3412c629beac9ae6aa7c63c123a66c7
Sachin Verma <139864259+ramasyasachin@users.noreply.github.com>
2023-08-23 22:05:09 +0530

    Add files via upload

---
+++ trending.js
@@ -0,0 +1,7 @@
+document.getElementById('trending1').innerHTML = ('Trend 1');
+
+document.getElementById('trending2').innerHTML = ('Trend 2');
+
+document.getElementById('trending3').innerHTML = ('Trend 3');
+
+document.getElementById('trending4').innerHTML = ('Trend 4');
