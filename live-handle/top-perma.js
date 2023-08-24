ba7d708e3307c1a50ec73fb2af5586ae5e480f26
Sachin Verma <139864259+ramasyasachin@users.noreply.github.com>
2023-08-24 15:56:22 +0530

    Add files via upload

---
+++ live-handle/top-perma.js
@@ -0,0 +1,45 @@
+document.getElementById('logo').src = 'images/untitled.png';
+
+
+
+document.getElementById('menu-op-1').innerHTML = ('Option-1');
+document.getElementById('menu-op-1').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-2').innerHTML = ('Option-2');
+document.getElementById('menu-op-2').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-3').innerHTML = ('Option-3');
+document.getElementById('menu-op-3').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-4').innerHTML = ('Option-4');
+document.getElementById('menu-op-4').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-5').innerHTML = ('Option-5');
+document.getElementById('menu-op-5').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-6').innerHTML = ('Option-6');
+document.getElementById('menu-op-6').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-7').innerHTML = ('Option-7');
+document.getElementById('menu-op-7').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-8').innerHTML = ('Option-8');
+document.getElementById('menu-op-8').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-9').innerHTML = ('Option-9');
+document.getElementById('menu-op-9').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-10').innerHTML = ('Option-10');
+document.getElementById('menu-op-10').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-11').innerHTML = ('Option-11');
+document.getElementById('menu-op-11').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-12').innerHTML = ('Option-12');
+document.getElementById('menu-op-12').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-contact').innerHTML = ('Contact Us');
+document.getElementById('menu-op-contact').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-about').innerHTML = ('About Us');
+document.getElementById('menu-op-about').href = 'https://www.youtube.com/';
