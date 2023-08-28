816f7bb98baa2d4c4120a4ed4b061d63b083d528
Sachin Verma <139864259+ramasyasachin@users.noreply.github.com>
2023-08-28 14:09:54 +0530

    Add files via upload

--- live-handle/top-perma.js
+++ live-handle/top-perma.js
@@ -26,17 +26,46 @@ document.getElementById('menu-op-7').href = 'https://www.youtube.com/';
 document.getElementById('menu-op-8').innerHTML = ('Option-8');
 document.getElementById('menu-op-8').href = 'https://www.youtube.com/';
 
-document.getElementById('menu-op-9').innerHTML = ('Option-9');
-document.getElementById('menu-op-9').href = 'https://www.youtube.com/';
 
-document.getElementById('menu-op-10').innerHTML = ('Option-10');
-document.getElementById('menu-op-10').href = 'https://www.youtube.com/';
 
-document.getElementById('menu-op-11').innerHTML = ('Option-11');
-document.getElementById('menu-op-11').href = 'https://www.youtube.com/';
 
-document.getElementById('menu-op-12').innerHTML = ('Option-12');
-document.getElementById('menu-op-12').href = 'https://www.youtube.com/';
+
+
+document.getElementById('menu-om-1').innerHTML = ('Option-1');
+document.getElementById('menu-om-1').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-2').innerHTML = ('Option-2');
+document.getElementById('menu-om-2').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-3').innerHTML = ('Option-3');
+document.getElementById('menu-om-3').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-4').innerHTML = ('Option-4');
+document.getElementById('menu-om-4').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-5').innerHTML = ('Option-5');
+document.getElementById('menu-om-5').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-6').innerHTML = ('Option-6');
+document.getElementById('menu-om-6').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-7').innerHTML = ('Option-7');
+document.getElementById('menu-om-7').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-8').innerHTML = ('Option-8');
+document.getElementById('menu-om-8').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-9').innerHTML = ('Option-9');
+//document.getElementById('menu-op-9').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-10').innerHTML = ('Option-10');
+//document.getElementById('menu-op-10').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-11').innerHTML = ('Option-11');
+//document.getElementById('menu-op-11').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-12').innerHTML = ('Option-12');
+//document.getElementById('menu-op-12').href = 'https://www.youtube.com/';
 
 document.getElementById('menu-op-contact').innerHTML = ('Contact Us');
 document.getElementById('menu-op-contact').href = 'https://www.youtube.com/';
