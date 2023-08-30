b478075e586569b923cdb07eb6a31de93ec3702a
Sachin Verma <139864259+ramasyasachin@users.noreply.github.com>
2023-08-30 15:11:07 +0530

    Delete live-handle directory

--- live-handle/trending.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('trending1').innerHTML = ('Trend 1');
-
-document.getElementById('trending2').innerHTML = ('Trend 2');
-
-document.getElementById('trending3').innerHTML = ('Trend 3');
-
-document.getElementById('trending4').innerHTML = ('Trend 4');
