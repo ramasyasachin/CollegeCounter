5745772e41dcc8fd6ab3606f31c6e9869e38502c
Sachin Verma <139864259+ramasyasachin@users.noreply.github.com>
2023-08-30 15:21:31 +0530

    Add files via upload

---
+++ live-handle/top-perma.js
@@ -0,0 +1,74 @@
+document.getElementById('logo').src = 'https://collegecounter.com/images/light-logo.png';
+
+
+
+document.getElementById('menu-op-1').innerHTML = ('IITs');
+document.getElementById('menu-op-1').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-2').innerHTML = ('IIITs');
+document.getElementById('menu-op-2').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-3').innerHTML = ('NITs');
+document.getElementById('menu-op-3').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-4').innerHTML = ('Exams');
+document.getElementById('menu-op-4').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-5').innerHTML = ('Counsellings');
+document.getElementById('menu-op-5').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-6').innerHTML = ('AIIMS');
+document.getElementById('menu-op-6').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-7').innerHTML = ('Medical Colleges');
+document.getElementById('menu-op-7').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-8').innerHTML = ('Scholarships');
+document.getElementById('menu-op-8').href = 'https://www.youtube.com/';
+
+
+
+
+
+
+document.getElementById('menu-om-1').innerHTML = ('IITs');
+document.getElementById('menu-om-1').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-2').innerHTML = ('NITs');
+document.getElementById('menu-om-2').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-3').innerHTML = ('IIITs');
+document.getElementById('menu-om-3').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-4').innerHTML = ('Exams');
+document.getElementById('menu-om-4').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-5').innerHTML = ('Counselling');
+document.getElementById('menu-om-5').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-6').innerHTML = ('AIIMS');
+document.getElementById('menu-om-6').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-7').innerHTML = ('Medical College');
+document.getElementById('menu-om-7').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-om-8').innerHTML = ('Scholarships');
+document.getElementById('menu-om-8').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-9').innerHTML = ('Option-9');
+//document.getElementById('menu-op-9').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-10').innerHTML = ('Option-10');
+//document.getElementById('menu-op-10').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-11').innerHTML = ('Option-11');
+//document.getElementById('menu-op-11').href = 'https://www.youtube.com/';
+
+//document.getElementById('menu-op-12').innerHTML = ('Option-12');
+//document.getElementById('menu-op-12').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-contact').innerHTML = ('Contact Us');
+document.getElementById('menu-op-contact').href = 'https://www.youtube.com/';
+
+document.getElementById('menu-op-about').innerHTML = ('About Us');
+document.getElementById('menu-op-about').href = 'https://www.youtube.com/';
